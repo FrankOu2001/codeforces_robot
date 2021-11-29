951abed0174bcca9cc9974ad196d869cf4f7d518
IsTuTouYa <yelotuz_0321@163.com>
2021-11-29 23:17:48 +0800

    finish get_user_info initially

--- plugins/codeforces_data/__init__.py
+++ plugins/codeforces_data/__init__.py
@@ -4,6 +4,7 @@ import urllib.request
 import urllib.parse
 
 from nonebot import on_command, CommandSession
+from plugins.time import get_last_login_time
 
 __plugin_name = 'codeforces_data'
 __plugin_usage__ = '拉取codeforces的相关数据'
@@ -14,15 +15,14 @@ async def get_request(url):
         request = urllib.request.urlopen(url, timeout=5)
         get = json.loads(request.read())
 
-        if get['status'] != 'OK':
-            return get['result']
+        print(get)
+        print(type(get['status']), get['status'])
+        if get['status'] == 'OK':
+            return get['result'][0]
         else:
-            return -1
+            return False
     except urllib.error.HTTPError as exception:
         print(exception.code, file=sys.stderr)
-    finally:
-        print(url, file=sys.stderr)
-        return -2
 
 
 @on_command('get_user_info', aliases=['查询', 'info'])
@@ -34,3 +34,20 @@ async def get_user_info(session: CommandSession):
 
     info = await get_request(user_url + user_name)
 
+    sent_message = ""
+    if not info:
+        sent_message = "%s不存在，请重新尝试" % user_name
+
+    else:
+        sent_message = """Name: {name}
+        Last visit: {time}
+        Rank: {rank}
+        Rating: {rating}
+        Score change in the last game: {scoreChange}
+        Max rating: {maxRating}
+        """.format(name=info['handle'],
+                   time=await get_last_login_time(info['lastOnlineTimeSeconds']),
+                   rank=info['rank'], rating=info['rating'],
+                   scoreChange='coming soon', maxRating=info['maxRating'])
+
+    await session.send(sent_message)
