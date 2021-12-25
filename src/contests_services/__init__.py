cb354dbc83d4802596450cca644a32363cc10910
frankou <wjyue2001@qq.com>
2021-12-25 21:27:23 +0800

    fix: 修复一个拉取比赛的小错误

--- src/contests_services/__init__.py
+++ src/contests_services/__init__.py
@@ -1,11 +1,11 @@
-import codeforces
-import nowcoder
-import atcoder
+import src.contests_services.nowcoder
+import src.contests_services.atcoder
+import src.contests_services.codeforces
 
 
-async def get_contest(contest_type: str = "all") -> list:
+async def get_contest(contest_type: str = "") -> list:
     contest = []
-    if contest_type == 'all':
+    if contest_type == '':
         await codeforces.get_contests(contest)
         await nowcoder.get_contests(contest)
         await atcoder.get_contests(contest)
