993c109c1181930a665e5234a5f5ff3183e92721
frankou <wjyue2001@qq.com>
2022-01-03 20:46:48 +0800

    update: 将比赛信息存储到数据库中

--- src/contests_services/__init__.py
+++ src/contests_services/__init__.py
@@ -1,29 +1,14 @@
-from . import codeforces, nowcoder, atcoder
+from .database import pull_data
 
 
 async def get_contest(contest_type: str) -> list:
-    """
-    获取各个平台的比赛
-
-    目前支持的平台:
-    codeforces
-    atcoder
-    nowcoder
-
-    :param contest_type:
-    :return:
-    """
     if contest_type == 'cf':
-        return await codeforces.get_contests()
+        return await pull_data('codeforces')
     elif contest_type == 'at':
-        return await atcoder.get_contests()
+        return await pull_data('atcoder')
     elif contest_type == 'nk':
-        return await nowcoder.get_contests()
+        return await pull_data('nowcoder')
     elif contest_type == '':
-        s = []
-        s.extend(await codeforces.get_contests())
-        s.extend(await atcoder.get_contests())
-        s.extend(await nowcoder.get_contests())
-        return s
+        return await pull_data('')
 
     return []
