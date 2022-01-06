2c2e30456024cdb1a1fd7e1689d2113a901d322a
frankou <wjyue2001@qq.com>
2022-01-06 21:24:36 +0800

    fix: 修复了忘记更新数据库的逻辑错误

--- src/contests_services/database.py
+++ src/contests_services/database.py
@@ -4,7 +4,7 @@ import sys
 import mariadb
 from datetime import datetime
 from nonebot import logger, require
-from . import codeforces, atcoder, nowcoder
+from src.contests_services import codeforces, atcoder, nowcoder
 
 
 def get_connect() -> mariadb.connection:
@@ -25,6 +25,34 @@ def get_connect() -> mariadb.connection:
         sys.exit(-1)
 
     return conn
+
+
+async def __get_contest__(contest_type: str) -> list:
+    """
+    爬取各个平台的比赛
+
+    目前支持的平台:
+    codeforces
+    atcoder
+    nowcoder
+
+    :param contest_type:
+    :return:
+    """
+    if contest_type == 'codeforces':
+        return await codeforces.get_contests()
+    elif contest_type == 'atcoder':
+        return await atcoder.get_contests()
+    elif contest_type == 'nowcoder':
+        return await nowcoder.get_contests()
+    elif contest_type == '':
+        s = []
+        s.extend(await codeforces.get_contests())
+        s.extend(await atcoder.get_contests())
+        s.extend(await nowcoder.get_contests())
+        return s
+
+    return []
 
 
 async def __update__(typename) -> bool:
@@ -64,34 +92,6 @@ async def __update__(typename) -> bool:
         return status
 
 
-async def __get_contest__(contest_type: str) -> list:
-    """
-    爬取各个平台的比赛
-
-    目前支持的平台:
-    codeforces
-    atcoder
-    nowcoder
-
-    :param contest_type:
-    :return:
-    """
-    if contest_type == 'cf':
-        return await codeforces.get_contests()
-    elif contest_type == 'at':
-        return await atcoder.get_contests()
-    elif contest_type == 'nk':
-        return await nowcoder.get_contests()
-    elif contest_type == '':
-        s = []
-        s.extend(await codeforces.get_contests())
-        s.extend(await atcoder.get_contests())
-        s.extend(await nowcoder.get_contests())
-        return s
-
-    return []
-
-
 async def update(typename) -> bool:
     if typename == '':
         return await __update__('codeforces') and \
@@ -123,20 +123,20 @@ async def pull_data(typename='') -> list:
         x = eval(i[0])
         x['contest_time'] = tuple(map(lambda it: datetime.strptime(it, '%Y-%m-%d %H:%M:%S'), x['contest_time']))
         data.append(x)
-
+    connect.close()
     return data
 
 
 scheduler = require("nonebot_plugin_apscheduler").scheduler
 
 
-@scheduler.scheduled_job("cron", id="database_update", hour='3, 15')
+@scheduler.scheduled_job("cron", id="database_update", hour='3')
 async def database_update():
     await update('')
 
 
 if __name__ == '__main__':
-    # asyncio.run(update(''))
-    asyncio.run(pull_data('codeforces'))
+    asyncio.run(update(''))
 else:
-    asyncio.get_event_loop().run_until_complete(update(''))
+    logger.info('update the contests data when launching the bot')
+    asyncio.run(update(''))
